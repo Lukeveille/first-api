7a0853572cc500acdd3d8a7aff08843acc763b27
Lukeveille <luke@lukeleveille.com>
2018-10-23 13:02:34 -0400

    able to return json

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,14 @@
 class ProductsController < ApplicationController
   def index
     @response = JSON.parse(HTTParty.get('http://lcboapi.com/products/?page=8').body)
+
+    respond_to do |format|
+      format.html {
+        render :index
+      }
+      format.json {
+        render json: @response
+      }
+    end
   end
 end
